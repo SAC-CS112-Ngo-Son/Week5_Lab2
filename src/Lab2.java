0e316850b94f2030a71e2dba69d34f37543f5bd6
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-09-23 21:58:52 -0700

    input strings

--- src/Lab2.java
+++ src/Lab2.java
@@ -7,18 +7,20 @@ public class Lab2
 	{
 		Scanner input = new Scanner(System.in);
 
-		int num1;
+		//int num1;
+		String ans1, ans2;
 
-		System.out.print("Are you hungry? (Enter 1 for YES, 0 for NO): ");
-		num1 = input.nextInt();
+		System.out.print("Are you hungry? (Enter YES or NO): ");
+		//num1 = input.nextInt();
+		ans1 = input.next();
 
-		if (num1 == 0)
+		if (ans1.equals("NO"))
 			System.out.println("OK. Bye!");
 		else
 		{
-			System.out.print("Get in line!\nLet's get some lunch!\nAre you thirsty? (Enter 1 for YES, 0 for NO): ");
-			num1 = input.nextInt();
-			if (num1 == 1)
+			System.out.print("Get in line!\nLet's get some lunch!\nAre you thirsty? (Enter YES or NO): ");
+			ans2 = input.next();
+			if (ans2.equals("YES"))
 				System.out.print("OK. Let's get some Coke!\nNow eat!\n");
 			else
 				System.out.print("OK. Let's get some water.\nNow eat!\n");
